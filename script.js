0fdec51c0cf2dd1af6f39bf8c0fa4c267da2a73f
chyppoli <claude.hyppolite@gmail.com>
2020-10-28 16:47:54 -0400

    redesign site. change to single page

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var elems = document.querySelectorAll(".sidenav");
-  var instances = M.Sidenav.init(elems, {});
-});
+var elem = document.querySelector('.sidenav');
+  var instance = new M.Sidenav(elem);
+
+   // with jquery
+
+//   $(document).ready(function(){
+//    $('.sidenav').sidenav();
+//   });
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.scrollspy');
+//     var instances = M.ScrollSpy.init(elems, options);
+//   });
+
+  $(document).ready(function(){
+    $('.scrollspy').scrollSpy();
+  });
